24d11c88439685affaf5821fb246e51be2d7b17e
Abhijit-Dev-app <solemrec@gmail.com>
2026-08-13 09:55:33 +0530

    Add: longest-increasing-subsequence - submission-1

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[]dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        if(nums.length ==  1){
+            return 1;
+        }
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < nums.length; i++){
+            for(int j = 0; j < i; j++){
+                if(nums[j] < nums[i]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+                max = Math.max(max, dp[i]);
+            }
+        }
+        return max;
+    }
+}
